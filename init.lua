ac30fa7ff3b80482d046b161a1c29ddd8f19c75f
Florian Mros <florian_alexander.mros@tu-dresden.de>
2023-12-07 18:51:14 +0100

    wip

--- init.lua
+++ init.lua
@@ -1,49 +1,48 @@
 -- set leader key to space
 vim.g.mapleader = " "
 
-local keymap = vim.keymap -- for conciseness
+local keymap = vim.api.nvim_set_keymap -- for conciseness
 
----------------------
--- General Keymaps -------------------
+----------------------- General Keymaps -------------------
 
 -- use jk to exit insert mode
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
 -- clear search highlights
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+keymap("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- Save the current buffer in normal mode
-keymap.set("n", "<leader>ss", ":w<CR>", { desc = "Quick Save in Normal Mode" })
+keymap("n", "<leader>ss", ":w<CR>", { desc = "Quick Save in Normal Mode" })
 
 -- Save the current buffer in visual mode and return to normal mode
-keymap.set("v", "<leader>ss", "<Esc>:w<CR>", { desc = "Quick Save in Visual Mode" })
+keymap("v", "<leader>ss", "<Esc>:w<CR>", { desc = "Quick Save in Visual Mode" })
 
 -- Copy to clipboard
-keymap.set("v", "<leader>cc", '"+y', { desc = "Copy to clipboard" })
+keymap("v", "<leader>cc", '"+y', { desc = "Copy to clipboard" })
 
 -- highlight under cursor
-keymap.set("n", "vv", "*", { desc = "Highlight word under cursor" })
+keymap("n", "vv", "*", { desc = "Highlight word under cursor" })
 
 -- delete single character without copying into register
-keymap.set("n", "x", '"_x')
+keymap("n", "x", '"_x')
 
 -- increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+keymap("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
+keymap("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>sr", ":vertical resize +5<CR>", { desc = "Increase window width" }) -- increase window width
-keymap.set("n", "<leader>sl", ":vertical resize -5<CR>", { desc = "Decrease window width" }) -- decrease window width
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap("n", "<leader>sr", ":vertical resize +5<CR>", { desc = "Increase window width" }) -- increase window width
+keymap("n", "<leader>sl", ":vertical resize -5<CR>", { desc = "Decrease window width" }) -- decrease window width
+keymap("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
 local opt = vim.opt -- for conciseness
 
